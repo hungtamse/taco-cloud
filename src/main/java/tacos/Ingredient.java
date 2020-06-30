d62c47dc6e93aaaa6775adaaa64ef42a5b4e1a43
tam.le@ptnglobalcorp.com <hungtam007>
2020-06-30 16:07:07 +0700

    ch3 working

---
+++ src/main/java/tacos/Ingredient.java
@@ -0,0 +1,16 @@
+package tacos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Ingredient {
+	private String id;
+	private String name;
+	private Type type;
+
+	public static enum Type {
+		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+	}
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS Ingredient(
+    id VARCHAR(4) NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    type VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Taco(
+    id IDENTITY,
+    name VARCHAR(50) NOT NULL,
+    createdAt TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Taco_Ingredients(
+    taco bigint NOT NULL,
+    ingredient VARCHAR(4) NOT NULL
+)
